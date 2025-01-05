37d4d1feee3694a3bb64e303b056236e5a924a7a
Wupxy <muhammad.122140193@student.itera.ac.id>
2025-01-05 13:10:09 +0700

    feat : edit wisata page

--- components/guest/wisata/scroll-wisata.tsx
+++ components/guest/wisata/scroll-wisata.tsx
@@ -68,3 +68,24 @@ const Section2: React.FC<SectionProps> = ({ scrollYProgress }) => {
     </motion.div>
   );
 };
+
+const Section3: React.FC<SectionProps> = ({ scrollYProgress }) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
+  return (
+    <motion.div
+      style={{ scale, rotate }}
+      className="sticky top-0 h-screen bg-[#C72626] text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]"
+    >
+      <p>Scroll Perspective</p>
+      <div className="flex gap-4">
+        <p>Section</p>
+        <div className="relative w-[12.5vw] h-[12.5vw]">
+          <Image src={"/1.jpg"} alt="Image 1" layout="fill" />
+        </div>
+        <p>Transition</p>
+      </div>
+    </motion.div>
+  );
+};
