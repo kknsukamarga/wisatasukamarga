ad8f4310394620d7edc701d6734220ab329daf7b
almajulioo <alma.122140057@student.itera.ac.id>
2025-01-18 12:26:25 +0700

    add google analytics api

---
+++ app/api/analytics/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { BetaAnalyticsDataClient } from "@google-analytics/data";
+
+const propertyId = "473679036"; // Replace with your GA4 property ID
+
+// Initialize the Analytics Data API client
+const analyticsDataClient = new BetaAnalyticsDataClient();
+
+export async function GET() {
+  try {
+    // Run the report
+    const [response] = await analyticsDataClient.runReport({
+      property: `properties/${propertyId}`,
+      dateRanges: [
+        {
+          startDate: "2025-01-01",
+          endDate: "today",
+        },
+      ],
+      dimensions: [
+        {
+          name: "city",
+        },
+      ],
+      metrics: [
+        {
+          name: "activeUsers",
+        },
+        {
+          name: "eventCount", // Include event count metric
+        },
+      ],
+    });
+
+    // Format the response
+    // @ts-ignore
+    const reportData = response.rows.map((row: any) => ({
+      city: row.dimensionValues[0]?.value,
+      activeUsers: row.metricValues[0]?.value,
+      eventCount: row.metricValues[1]?.value,
+    }));
+
+    // Send the report data as JSON
+    return NextResponse.json(reportData, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching analytics data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch analytics data" },
+      { status: 500 }
+    );
+  }
+}
